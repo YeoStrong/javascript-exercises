af525c49e9032e87f66e0ee2bb92d51172a8bb79
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-20 13:19:03 +0100

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return 'ERROR'
+  }
+  if (a < b) { 
+    return ((b*(b+1)/2) - (a*(a-1)/2))
+  }  else if (a > b) {
+    return ((a*(a+1)/2) - (b*(b-1)/2))
+  } else if (a == b) {
+    return a;
+  } 
 };
 
 // Do not edit below this line

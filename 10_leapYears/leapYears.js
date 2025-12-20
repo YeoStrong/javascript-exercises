b409036dee2c2a97e5fa03be11935e18bf001369
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-20 17:02:33 +0100

    Complete 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 == 0 && year % 100 != 0) {
+    return true;
+  } else if (year % 400 == 0) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+// year % 4 == 0 => leapyear, year % 100 == 0 => no leapyear, year % 400 == 0 => leapyear

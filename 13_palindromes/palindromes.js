b7e18c59d190f9efafac4e09c2c04fd4c9fddb3e
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-30 13:00:50 +0100

    feat: add editorconfig and solve 13_pailindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const processed = str.toLowerCase().replace(/[^a-z0-9]/g, ""); // make str 'flat'
+  return processed === processed.split("").reverse().join(""); // to use .reverse()
 };
 
 // Do not edit below this line

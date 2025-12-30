9da7611dcafe26c2507556f265475224463270fa
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-30 13:56:48 +0100

    feat: solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+// using sort()
+// const findTheOldest = function (arr) {
+//   return arr.sort((a, b) => {
+//     const oldest = getAge(a.yearOfBirth, a.yearOfDeath);
+//     const current = getAge(b.yearOfBirth, b.yearOfDeath);
+//     return current - oldest;
+//   })[0];
+// };
 
+// using reduce()
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+    return oldestAge < currentAge ? current : oldest;
+  });
+};
+
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line

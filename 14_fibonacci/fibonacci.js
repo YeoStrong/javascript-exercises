3e5d9bc751843fd2f78acf728341d52137123bc6
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-30 13:15:27 +0100

    feat: solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num === 0) return 0;
+  if (num < 0) return "OOPS";
 
+  const fib = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib[num];
 };
+
+// 0 1 1 2 3 5 8 13
 
 // Do not edit below this line
 module.exports = fibonacci;

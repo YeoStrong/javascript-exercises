7c63b4182f99d243450e911791bcb4edf30d30a4
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-30 12:45:15 +0100

    feat: solve 13_factorial

--- 13_factorial/factorial.js
+++ 13_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (num) {
+  if (num === 0) return 1;
+  if (num < 0 || Number.isInteger(num) == false) return undefined;
+  let result = 1n;
+  for (let i = BigInt(num); i > 0; i--) {
+    result *= i;
+  }
+  return Number(result);
 };
 
 // Do not edit below this line
